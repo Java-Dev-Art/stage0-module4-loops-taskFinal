a72f89b6c7b05d8917aca660a6d4b6231199be11
Artur Markouski <markouski.artur@student.ehu.lt>
2024-07-20 13:57:39 +0200

    feat: add solv 5 final tasks

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        if (power < 4){
+            int result = (int)(Math.pow(numberToPrint, power));
+            System.out.println(result);
+            return;
+        }
+        int valueABS = Math.abs(numberToPrint);
+        int result = (int)(Math.pow(valueABS, power));
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
